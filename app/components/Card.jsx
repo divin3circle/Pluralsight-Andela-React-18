474454dfa56d662e09fbd327c30286cea90d7556
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-08-14 22:00:44 +0300

    finally

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,56 +1,75 @@
-"use client";
-import { useState } from "react";
-import Input from "./Input";
-import User from "./User";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "../styles.css";
-import { useRef } from "react";
+"use client"
 import axios from "axios";
+import { useState } from "react";
 
 const Card = () => {
-  const testData = [
-    {
-      name: "Sylus",
-      avatar_url:
-        "https://i.pinimg.com/564x/22/d5/b7/22d5b72cd6405a5ae2edc4feacdd08cd.jpg",
-      company: "divin3circle",
-    },
-    {
-      name: "Abel",
-      avatar_url:
-        "https://i.pinimg.com/564x/9f/02/8e/9f028e1344143cc6d2cef238442f12e6.jpg",
-      company: "sam77",
-    },
-  ];
-  const [input, setInput] = useState("");
-  const [users, setUsers] = useState([testData]);
-  const inputRef = useRef("");
+    const testData = [
+      {
+        login: "Sylus",
+        company: "@divin3circle",
+        avatar_url:
+          "https://i.pinimg.com/originals/d5/65/4b/d5654bd984020c62201eed2fb5896fce.jpg",
+      },
+      {
+        login: "Abel",
+        company: "@sourcecode",
+        avatar_url:
+          "https://i.pinimg.com/564x/22/d5/b7/22d5b72cd6405a5ae2edc4feacdd08cd.jpg",
+      },
+    ];
+    const [input, setInput] = useState("");
+    const [users, setUsers] = useState(testData);
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-    const resp = await axios.get(`https://api.github.com/users/${input}`);
-    toast.success(`${input} found!`, {
-      position: toast.POSITION.TOP_CENTER,
-      className: "toast-success",
-    });
-    setUsers(resp.data);
-    console.log(resp.data);
-    setInput("");
-  };
-  /* const profile = testData[0]; */
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const resp = await axios.get(`https://api.github.com/users/${input}`);
+        setUsers([...users, resp.data]);
+        console.log(users);
+    }
+
   return (
-    <div className="">
-      <Input input={input}  inputRef={inputRef} handleClick={handleClick} setInput={setInput} />
-      {testData.map((profile) => (
-        <User
-          key={profile.company}
-          name={profile.name}
-          img={profile.avatar_url}
-          usr={profile.company}
-        />
-      ))}
-      <ToastContainer />
+    <div>
+      <div>
+        <form
+          className="flex flex-row gap-4 items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="p-2 focus:outline-2 
+           rounded-lg focus:outline-teal-500 focus:border-b-2 font-semibold "
+            placeholder="Github username"
+          />
+          <button className="bg-teal-400 p-[0.5rem] rounded-lg border-teal-400 border-2 hover:bg-transparent ease-in duration-100">
+            Add User
+          </button>
+        </form>
+      </div>
+      {/**card */}
+      <div>
+        {users?.map((user, index) => (
+          <div key={index} className="flex justify-center mx-auto mt-5">
+            {/**avatar */}
+            <div className="flex justify-center gap-4 rounded-lg hover:bg-teal-100 hover:scale-105 ease-in duration-150 cursor-pointer  md:w-[42rem] ">
+              <div>
+                <img
+                  src={user.avatar_url}
+                  alt="login"
+                  className=" object-cover rounded-full h-[100px] w-[100px]"
+                />
+              </div>
+              <div className="p-2">
+                <h1 className="font-bold text-lg">{user.login}</h1>
+                <h2 className="font-light leading-relaxed text-teal-600">
+                  {user.company}
+                </h2>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import Card from './components/Card';
+import Card from "./components/Card";
+
 
 export default function Home() {
   return (
     <>
-      <h1 className=" text-red-500 text-xl">Home</h1>
+      <h1 className="text-center font-bold m-4 text-red-500 text-xl">Github Cards</h1>
       <Card />
     </>
   );
